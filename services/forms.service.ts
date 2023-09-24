91cb48fd95ada7994edb064e273637f9387d6e6e
x57 <projeta618@gmail.com>
2023-09-24 19:06:12 -0300

    Init the first commit

---
+++ services/forms.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from "@angular/core";
+// import '@angulars/localize/init';
+
+var $localize = (msg: any) => msg;
+var $localize = function(data: any) { return data };
+
+const FORM_DJANGO_TRADUCTOR: any = {
+  email: {
+    exists: $localize`This email address is already in use.`,
+    invalid: $localize`The email address is invalid.`,
+    required: $localize`This field is required.`,
+    not_encountered: $localize`This email address is not registered.`,
+    'Enter a valid email address.': $localize`The email address is invalid.`,
+    invalid_partner_email: $localize`The email address is invalid for this partner.`,
+  },
+  password: {
+    password_is_alpha: $localize`Password must contain at least one number.`,
+    password_is_numeric: $localize`Password must contain at least one letter.`,
+    password_too_long: $localize`Password must contain at most 128 characters.`,
+    password_too_short: $localize`Password must contain at least 8 characters.`,
+    password_is_lower: $localize`Password must contain at least one uppercase letter.`,
+    password_has_space: $localize`Password must not contain spaces.`,
+    required: $localize`This field is required.`,
+  },
+  collegeCode: {
+    invalid: $localize`The college code is invalid.`,
+    required: $localize`This field is required.`,
+  },
+  first_name: {
+    required: $localize`This field is required.`,
+  },
+  last_name: {
+    required: $localize`This field is required.`,
+  },
+  __signin__: {
+    not_exists: $localize`Please fill in your email and password to sign in`,
+  },
+};
+
+@Injectable({
+  providedIn: "root",
+})
+export class FormsService {
+  /**
+   * @description: Constructeur.
+   */
+  constructor() {}
+
+  /**
+   * @description: Retourne le message d'erreur associé à un champ de formulaire.
+   */
+  public hasError(httpResponse: any, inputName: string | undefined): boolean {
+    if (httpResponse === undefined) {
+      return false;
+    }
+
+    if (inputName === undefined) {
+      return false;
+    }
+    return (
+      httpResponse.form_error !== undefined &&
+      httpResponse.form_error[inputName] !== undefined
+    );
+  }
+
+  /**
+   * @description: Retourne le message d'erreur associé à un champ de formulaire.
+   */
+  public notHasError(
+    httpResponse: any,
+    inputName: string | undefined
+  ): boolean {
+    return !this.hasError(httpResponse, inputName);
+  }
+
+  /**
+   * @description: Retourne le message d'erreur associé à un champ de formulaire.
+   * @param httpResponse: any
+   * @param inputName: string | undefined
+   * @param traductor: any
+   */
+  public getErrorMsg(
+    httpResponse: any,
+    inputName: string | undefined
+  ): string | undefined {
+    if (this.notHasError(httpResponse, inputName)) {
+      return undefined;
+    }
+
+    if (inputName === undefined) {
+      return undefined;
+    }
+
+    const error = httpResponse.form_error[inputName][0];
+    const errorMsg = this.getErrorMsgDjango(error, inputName);
+    if (errorMsg === undefined) {
+      console.error("FORM-ERROR>>>>", error);
+    }
+    return errorMsg;
+  }
+
+  /**
+   * @description: Retourne le message d'erreur associé à un champ de formulaire.
+   * @param httpResponse: any
+   * @param inputName: string | undefined
+   */
+  private getErrorMsgDjango(
+    error: string,
+    inputName: string
+  ): string | undefined {
+    if (FORM_DJANGO_TRADUCTOR.hasOwnProperty(inputName) === false) {
+      return undefined;
+    }
+
+    if (FORM_DJANGO_TRADUCTOR[inputName].hasOwnProperty(error) === false) {
+      return undefined;
+    }
+
+    return FORM_DJANGO_TRADUCTOR[inputName][error];
+  }
+}
