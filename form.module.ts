2c604ce3333ebd509338d3cbb13cb189560ab8c8
x57 <projeta618@gmail.com>
2023-09-24 19:12:05 -0300

    Init the first commit

--- form.module.ts
+++ form.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormErrorComponent } from './components/form-error/form-error.component';
+import { TextareaComponent } from './components/textarea/textarea.component';
+import { SubmitComponent } from './components/submit/submit.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { FilterComponent } from './components/filter/filter.component';
 
+import { FormsService } from './services/forms.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormErrorComponent,
+    TextareaComponent,
+    SubmitComponent,
+    CheckboxComponent,
+    FilterComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    FormsService,
+  ],
+  exports: [
+    FormErrorComponent,
+    TextareaComponent,
+    SubmitComponent,
   ]
 })
 export class FormModule { }
